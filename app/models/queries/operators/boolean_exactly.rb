5e7ca411ae9d42df9aaeef344ee3adecb6973059
Kerbrat <jbk_83@hotmail.com>
2022-09-25 14:21:41 -0400

    1186-1187 Update filter multiple field

--- app/models/queries/operators/boolean_exactly.rb
+++ app/models/queries/operators/boolean_exactly.rb
@@ -1,6 +1,6 @@
 #-- copyright
 # OpenProject is an open source project management software.
-# Copyright (C) 2012-2023 the OpenProject GmbH
+# Copyright (C) 2012-2022 the OpenProject GmbH
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License version 3.
@@ -26,17 +26,14 @@
 # See COPYRIGHT and LICENSE files for more details.
 #++
 
-module Queries::Filters::Strategies
-  class CfListOptional < ListOptional
-    private
+module Queries::Operators
+  class BooleanExactly < Base
+    label 'equals_exactly'
+    set_symbol '=='
 
-    def operator_map
-      super_value = super.dup
-      super_value['!*'] = ::Queries::Operators::NoneOrBlank
-      super_value['*'] = ::Queries::Operators::AllAndNonBlank
-      super_value['=='] = ::Queries::Operators::BooleanExactly
-
-      super_value
+    def self.sql_for_field(values, db_table, db_field)
+      "1=1 GROUP BY work_packages.id having array_agg(#{db_table}.#{db_field}) = '{" +
+        values.map { |val| "\"#{connection.quote_string(val)}\"" }.join(',') + '}\''
     end
   end
 end
