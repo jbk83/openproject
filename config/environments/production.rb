7517f35c317c924ea4ade01fc183a04ed89c0626
Kerbrat <jbk_83@hotmail.com>
2022-09-25 21:44:38 -0400

    Update config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ OpenProject::Application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = nil
